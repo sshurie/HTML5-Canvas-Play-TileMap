c1450df4c9d064bc3e38352505d91a808dcba1af
Samantha Shurie <s_shurie@yahoo.com>
2016-07-07 01:15:07 -0400

    fixed Math.round to Math.floor in drawTile function

--- canvas.js
+++ canvas.js
@@ -31,8 +31,7 @@ var map = {
 				  170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170,
 				  171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171, 170, 171],
 				  
-	land_tiles: [ 
-				  null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+	land_tiles: [ null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
 				  null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
 				  null, null, null, null, null, null, null, null, 196, 197, 197, 197, 197, 197, 197, 197, 197, 197, 197, 197, 198, null, null, null, null,
 				  null, null, null, null, null, null, null, null, 203, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 2, 205, null, null, null, null,
@@ -58,49 +57,51 @@ var map = {
 				  null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
 				  null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,],
 				  
-		object_tiles: [ null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-				  null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+		object_tiles: [ 
+					null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+					null, null, 195, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
 				  null, null, null, null, null, null, null, null, null, null, null, null,null, null, null, null, null, null, null, null, null, null, null, null, null,
-				  null, null, null, null, null, null, null, null, null, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 2, null, null, null, null, null,
-				  null, null, null, null, null, null, null, null, 203, 3, 0, 1, 2, 1, 0, 1, 0, 1, 0, 1, null, null, null, null, null,
-				  null, null, null, null, null, null, null, null, 203, 0, 1, 0, 1, 0, 3, 0, 1, 0, 3, 0, null, null, null, null, null,
-				  null, null, 196, 197, 197, 197, 197, 197, 191, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, null, null, null, null, null,
-				  null, null, 203, 0, 1, 2, 1, 0, 1, 0, 1, 0, 1, 2, 1, 0, 1, 0, 3, 0, 189, 197, null, null, null,
-				  null, null, 203, 1, 0, 1, 0, 1, 0, 1, 0, 1, 70, 71, 71, 71, 72, 1, 0, 1, 0, 1, null, null, null,
-				  null, null, 203, 0, 1, 0, 1, 0, 3, 0, 1, 2, 1, 0, 46, 47, 79, 0, 1, 0, 1, 0, null, null, null,
-				  null, null, 203, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 53, 54, 79, 1, 0, 1, 0, 1, null, null, null, 
-				  null, null, 203, 0, 1, 0, 1, 0, 3, 0, 1, 21, 1, 0, 1, 0, 86, 0, 1, 0, 1, 0, null, null, null, 
-				  null, null, 203, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, null, null, null,
-				  null, null, 203, 0, 1, 0, 1, 0, 3, 2, 1, 2, 1, 0, 1, 0, 3, 0, 1, 0, 1, 0, 205, null, null,
-				  null, null, 203, 1, 0, 1, 0, 1, 2, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 205, null, null, 
-				  null, null, 203, 0, 1, 0, 1, 0, 3, 0, 1, 2, 1, 0, 1, 0, 3, 0, 1, 0, 1, 3, 205, null, null,
-				  null, null, 203, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 2, 1, 0, 1, 205, null, null,
-				  null, null, 203, 0, 1, 0, 1, 0, 3, 0, 1, 2, 1, 0, 1, 0, 3, 0, 2, 3, 1, 0, 205, null, null,
-				  null, null, 210, 211, 211, 211, 211, 211, 211, 211, 211, 177, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 205, null, null, null,
-				  null, null, null, null, null, null, null, null, null, null, 203, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 205, null, null, null,
-				  null, null, null, null, null, null, null, null, null, null, 203, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 205, null, null, null,
-				  null, null, null, null, null, null, null, null, null, null, 203, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 205, null, null, null,
-				  null, null, null, null, null, null, null, null, null, null, 210, 211, 211, 211, 211, 211, 211, 211, 211, 211, 211, 212, null, null, null,
-				  null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+				  null, null, null, null, null, null, null, null, null, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 2, 188, null, null, null, null,
+				  null, null, null, null, null, null, null, null, null, 3, 0, 1, 2, 1, 0, 1, 0, 1, 0, 1, null, null, 195, null, null,
+				  null, null, null, null, null, null, null, 238, null, 0, 1, 0, 1, 0, 3, 0, 1, 0, 3, 0, null, null, null, null, null,
+				  null, null, null, null, null, null, null, null, null, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, null, null, null, null, null,
+				  null, null, null, 0, 1, 2, 1, 0, 1, 0, 1, 0, 1, 2, 1, 0, 1, 0, 3, 0, null, null, null, null, null,
+				  null, null, null, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 91, 92, 92, 93, 1, 0, 1, 0, 1, null, null, null,
+				  null, null, null, 0, 1, 0, 1, 0, 3, 0, 1, 2, 1, 0, 51, 52, 100, 0, 1, 0, 1, 0, null, null, null,
+				  null, null, null, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 51, 52, 100, 1, 0, 1, 0, 1, null, null, null, 
+				  null, null, null, 0, 1, 0, 1, 0, 3, 0, 1, 21, 1, 0, 1, 92, 107, 0, 1, 0, 1, 0, null, null, null, 
+				  null, null, null, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, null, null, null,
+				  null, null, null, 0, 1, 0, 1, 0, 3, 2, 1, 2, 1, 0, 1, 0, 3, 0, 1, 0, 1, 0, null, null, null,
+				  null, null, null, 1, 0, 1, 0, 1, 2, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, null, null, null, 
+				  null, null, null, 0, 1, 0, 1, 0, 3, 0, 1, 2, 1, 0, 1, 0, 3, 0, 1, 0, 1, 3, null, null, null,
+				  null, null, null, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 2, 1, 0, 1, null, null, null,
+				  null, null, null, 0, 1, 0, 1, 0, 3, 0, 1, 2, 1, 0, 1, 0, 3, 0, 2, 3, 1, 0, null, null, null,
+				  null, null, null, null, 241, null, null, 188, null, null, null, null, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, null, null, null,
+					null, null, null, null, null, null, null, null, null, null, null, null, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, null, null, null,
+				  null, null, 239, null, null, null, null, null, null, null, null, null, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, null, null, null,
+				  null, null, null, null, null, null, null, null, null, null, null, null, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, null, null, null,
+				  null, null, null, null, null, 195, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+				  null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 238, null,
 				  null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
 	
 	getTile: function(arr, row, col){
-		if(arr === "water_tiles") {
+		if(arr === "water") {
 			return this.water_tiles[row * map.cols + col];
-		} else if(arr === "land_tiles") {
+		} else if(arr === "land") {
 			return this.land_tiles[row * map.cols + col];
-		} else if(arr === "object_tiles") {
+		} else if(arr === "object") {
 			return this.object_tiles[row * map.cols + col];
 		}
 		
 	},
 	
-	// row and column specify the desired location of the tile
+	// tile: number
+	//row and column: specify the desired location of the tile
 	drawTile: function(tile, col, row) {
 		var frame = document.getElementById('window');
 		var ctx = frame.getContext('2d');
 		// find source row and column position
-		var r = Math.round(tile / 7);
+		var r = Math.floor(tile / 7);
 		var c = tile % 7;
 		var x = c * map.tsize;
 		var y = r * map.tsize;
@@ -123,13 +124,13 @@ function setUp() {
 	var ctx = frame.getContext('2d');
 	for(var col = 0; col < map.cols; col++) {
 		for(var row = 0; row < map.rows; row++) {
-			var tile = map.getTile("water_tiles", col, row);
+			var tile = map.getTile("water", col, row);
 			map.drawTile(tile, col, row);
-			tile = map.getTile("land_tiles", col, row);
+			tile = map.getTile("land", col, row);
 			if(tile !== null) {
 				map.drawTile(tile, col, row);
 			}	
-			tile = map.getTile("object_tiles", col, row);
+			tile = map.getTile("object", col, row);
 			if(tile !== null) {
 				map.drawTile(tile, col, row);
 			}
